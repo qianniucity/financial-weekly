6eea23a9f7401e249ab657ee492b2b49979608be
king101125s@163.com <king101125s@gmail.com>
2024-05-08 13:30:37 +0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+## 安装 ollama
+- 下载地址：https://ollama.com/
+- Github：https://github.com/ollama/ollama
+下载 llama3:8b
+```shell
+ollama pull llama3:8b
+```
---
+++ src/main/java/city/qianniu/RagWorker.java
@@ -0,0 +1,29 @@
+package city.qianniu;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.ai.reader.tika.TikaDocumentReader;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RagWorker {
+
+    private final VectorStore vectorStore;
+    private final Resource pdfResource;
+
+    public RagWorker(VectorStore vectorStore,
+            @Value("classpath:/H3_AP202404221630957432_1.pdf") Resource pdfResource) {
+        this.vectorStore = vectorStore;
+        this.pdfResource = pdfResource;
+    }
+
+    @PostConstruct
+    public void init() {
+        TikaDocumentReader tikaDocumentReader = new TikaDocumentReader(pdfResource);
+        TokenTextSplitter tokenTextSplitter = new TokenTextSplitter();
+        vectorStore.add(tokenTextSplitter.apply(tikaDocumentReader.get()));
+    }
+}
